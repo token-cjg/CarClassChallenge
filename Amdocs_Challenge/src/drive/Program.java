754769215defd16cab53c7f02608e2b4aa9c3782
Roderick Shapeley <roderickshapeley@gmail.com>
2015-05-08 17:24:29 +1000

    pushing non static component of program to external class

---
+++ Amdocs_Challenge/src/drive/Program.java
@@ -0,0 +1,13 @@
+package drive;
+
+public class Program {
+	public Car car = null;
+	
+	public static void main(String [] args)
+	{
+		Executor executor = new Executor();
+		executor.ReadInputFile("input.txt");
+	}
+	
+	
+}
